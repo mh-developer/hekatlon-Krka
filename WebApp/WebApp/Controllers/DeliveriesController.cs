0b51c172d972297d0aef89a24e4ce2bf44d02f83
Mitja <mh7860@student.uni-lj.si>
2020-04-04 18:24:04 +0200

    feat: init controllers for Account & Deliveries & Warehouses

---
+++ WebApp/WebApp/Controllers/DeliveriesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Domain.Models;
+using WebApp.Infrastructure;
+
+namespace WebApp.Controllers
+{
+    public class DeliveriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DeliveriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Deliveries
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Deliveries.ToListAsync());
+        }
+
+        // GET: Deliveries/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var delivery = await _context.Deliveries
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (delivery == null)
+            {
+                return NotFound();
+            }
+
+            return View(delivery);
+        }
+
+        // GET: Deliveries/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Deliveries/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Code,Status,DispatchTime,DeliveryTime,CreationTime,Id,IsDeleted,DeletionTime")] Delivery delivery)
+        {
+            if (ModelState.IsValid)
+            {
+                delivery.Id = Guid.NewGuid();
+                _context.Add(delivery);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(delivery);
+        }
+
+        // GET: Deliveries/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var delivery = await _context.Deliveries.FindAsync(id);
+            if (delivery == null)
+            {
+                return NotFound();
+            }
+            return View(delivery);
+        }
+
+        // POST: Deliveries/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Code,Status,DispatchTime,DeliveryTime,CreationTime,Id,IsDeleted,DeletionTime")] Delivery delivery)
+        {
+            if (id != delivery.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(delivery);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DeliveryExists(delivery.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(delivery);
+        }
+
+        // GET: Deliveries/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var delivery = await _context.Deliveries
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (delivery == null)
+            {
+                return NotFound();
+            }
+
+            return View(delivery);
+        }
+
+        // POST: Deliveries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var delivery = await _context.Deliveries.FindAsync(id);
+            _context.Deliveries.Remove(delivery);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DeliveryExists(Guid id)
+        {
+            return _context.Deliveries.Any(e => e.Id == id);
+        }
+    }
+}
